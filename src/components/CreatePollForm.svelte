a083cae45fb1e97ad4efa534cec98d0e4b331284
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-01-16 12:28:39 +0100

    Completed Poll Form Component

---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    let dispatch = createEventDispatcher();
+
+    let fields = {
+        question: '',
+        answerA: '',
+        answerB: ''
+    };
+
+    const submit = () => {
+        dispatch('', fields);
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+    <div class="form-field">
+        <label for="question">Poll Question</label>
+        <input type="text" id="question" bind:value={fields.question}>
+    </div>
+    <div class="form-field">
+        <label for="answer-a">Answer A</label>
+        <input type="text" id="answer-a" bind:value={fields.answerA}>
+    </div>
+    <div class="form-field">
+        <label for="answer-b">Answer B:</label>
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
+    </div>
+    <button class="btn btn-success">Add Poll</button>
+</form>
+
+<style>
+    form{
+        width: 400px;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    .form-field{
+        margin: 18px auto;
+    }
+    input{
+        width: 100%;
+        border-radius: 6px;
+    }
+    label{
+        margin: 10px auto;
+        text-align: left;
+    }
+</style>
